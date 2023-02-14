aef95afa19b1a4bdb053579b9a88d57baae812f6
smichaelonline <stephaniemichael001@gmail.com>
2023-02-13 20:36:22 -0500

    Initial Commit

---
+++ Stephs-Recs/Stephs_RecsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stephs_RecsApp.swift
+//  Stephs-Recs
+//
+//  Created by Stephanie Michael on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Stephs_RecsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
